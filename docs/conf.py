a4b7fad99e541b30ed68a1bfbf536a7bb58eb243
yakimka <ss.yakim@gmail.com>
2024-06-19 16:29:52 +0300

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "furo"
 html_static_path = ["_static"]
+html_logo = "_static/picodi_logo.png"
 
 
 signatures_for_replace = {
