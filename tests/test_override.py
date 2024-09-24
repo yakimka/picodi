608824c63841e863dc79fef121329d8969b00e17
yakimka <ss.yakim@gmail.com>
2024-09-24 17:33:03 +0300

    Add pragma no cover

--- tests/test_override.py
+++ tests/test_override.py
@@ -99,10 +99,10 @@ def test_can_context_manager_return_state_to_previous_not_to_original():
 def test_overriding_overridden_dependency_dont_allowed_and_must_raise_error():
     @inject
     def my_service(settings: dict = Provide(get_abc_settings)):
-        return settings
+        return settings  # pragma: no cover
 
     def first_override():
-        return {"first": "override"}
+        return {"first": "override"}  # pragma: no cover
 
     def second_override():
         return {"second": "override"}  # pragma: no cover
@@ -210,7 +210,7 @@ async def test_can_use_async_dep_with_not_default_scope_in_override_in_sync_cont
 
 def test_cant_override_dependency_with_itself():
     def get_settings() -> dict:
-        return {"default": "settings"}
+        return {"default": "settings"}  # pragma: no cover
 
     with pytest.raises(ValueError, match="Cannot override a dependency with itself"):
         registry.override(get_settings, get_settings)
@@ -219,7 +219,7 @@ def test_cant_override_dependency_with_itself():
 def test_can_override_with_injected_dep():
     @inject
     def original_dep(num: int = Provide(lambda: 1)):
-        return num
+        return num  # pragma: no cover
 
     @inject
     def overriding_dep(num2: int = Provide(lambda: 42)):
@@ -238,7 +238,7 @@ def test_can_override_with_injected_dep():
 def test_can_override_with_zero_arguments_function():
     @inject
     def original_dep(num: int = Provide(lambda: 1)):
-        return num
+        return num  # pragma: no cover
 
     def overriding_dep():
         return 42
@@ -256,7 +256,7 @@ def test_can_override_with_zero_arguments_function():
 async def test_can_override_with_injected_dep_async():
     @inject
     async def original_dep(num: int = Provide(lambda: 1)):
-        return num
+        return num  # pragma: no cover
 
     @inject
     async def overriding_dep(num2: int = Provide(lambda: 42)):
@@ -275,7 +275,7 @@ async def test_can_override_with_injected_dep_async():
 async def test_can_override_with_zero_arguments_function_async():
     @inject
     async def original_dep(num: int = Provide(lambda: 1)):
-        return num
+        return num  # pragma: no cover
 
     async def overriding_dep():
         return 42
@@ -293,7 +293,7 @@ async def test_can_override_with_zero_arguments_function_async():
 def test_can_override_deeply_nested_dep():
     @inject
     async def original_dep(num: int = Provide(lambda: 1)):
-        return num
+        return num  # pragma: no cover
 
     def overriding_dep():
         return 42
