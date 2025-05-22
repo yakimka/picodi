ab56788a98908d390a9e793bea89a77703f03b4e
yakimka <ss.yakim@gmail.com>
2025-05-23 01:10:56 +0300

    Clean test

--- tests/core/test_injectless_dependencies.py
+++ tests/core/test_injectless_dependencies.py
@@ -1,17 +1,13 @@
-from picodi import NullScope, Provide, inject, registry
+from picodi import Provide, inject
 
 
 def test_can_resolve_dependencies_without_inject():
-
-    @registry.set_scope(NullScope)
     def dep_a():
         return "a"
 
-    @registry.set_scope(NullScope)
     def dep_b(a: str = Provide(dep_a)):
         return a + "b"
 
-    @registry.set_scope(NullScope)
     def dep_c(b: str = Provide(dep_b)):
         return b + "c"
 
