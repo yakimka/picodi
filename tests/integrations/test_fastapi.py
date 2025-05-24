333b70f977b2e60ec28ea1372a17247d1c4a244b
yakimka <ss.yakim@gmail.com>
2025-05-25 00:42:51 +0300

    Add tests

--- tests/integrations/test_fastapi.py
+++ tests/integrations/test_fastapi.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Annotated
 
 import pytest
@@ -310,3 +311,67 @@ async def test_middleware_init_and_shutdown_request_scope_sync(
 
     assert init_counter == 1
     assert closing_counter == 1
+
+
+async def test_can_use_request_scope_dependency_async(make_app, make_asgi_client):
+    init_counter = 0
+    closing_counter = 0
+
+    @registry.set_scope(RequestScope)
+    async def get_42():
+        nonlocal init_counter
+        init_counter += 1
+        yield 42
+        nonlocal closing_counter
+        closing_counter += 1
+
+    app = make_app()
+
+    @app.get("/")
+    async def root(dep: int = Provide(get_42, wrap=True)):
+        return {"dep": dep}
+
+    async with make_asgi_client(app) as asgi_client:
+        await asyncio.gather(
+            asgi_client.get("/"),
+            asgi_client.get("/"),
+            asgi_client.get("/"),
+            asgi_client.get("/"),
+        )
+        resp = await asgi_client.get("/")
+
+    assert resp.json() == {"dep": 42}
+    assert init_counter == 5
+    assert closing_counter == 5
+
+
+async def test_can_use_request_scope_dependency_sync(make_app, make_asgi_client):
+    init_counter = 0
+    closing_counter = 0
+
+    @registry.set_scope(RequestScope)
+    def get_42():
+        nonlocal init_counter
+        init_counter += 1
+        yield 42
+        nonlocal closing_counter
+        closing_counter += 1
+
+    app = make_app()
+
+    @app.get("/")
+    def root(dep: int = Provide(get_42, wrap=True)):
+        return {"dep": dep}
+
+    async with make_asgi_client(app) as asgi_client:
+        await asyncio.gather(
+            asgi_client.get("/"),
+            asgi_client.get("/"),
+            asgi_client.get("/"),
+            asgi_client.get("/"),
+        )
+        resp = await asgi_client.get("/")
+
+    assert resp.json() == {"dep": 42}
+    assert init_counter == 5
+    assert closing_counter == 5
